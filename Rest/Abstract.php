1752ff5ff5906a1e452728f01bb5919c0e4580a4
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-10-03 14:20:21 +0000

    refactoring

--- Rest/Abstract.php
+++ Rest/Abstract.php
@@ -1,86 +1,76 @@
 <?php
-require_once('Exception.php');
-require_once('CurlClient.php');
+/**
+ * @see Alfresco_Rest_Exception
+ */
+require_once 'Exception.php';
 
+/**
+ * @see Alfresco_Rest_CurlClient
+ */
+require_once 'CurlClient.php';
+
+/**
+ * Base class for the REST services
+ * 
+ * @author Gil Magno <gilmagno@gmail.com>
+ * @package Alfresco-PHP
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License 3
+ */
 abstract class Alfresco_Rest_Abstract
 {
-    const DEFAULT_ADAPTER = 'json';
-    
     private $_baseUrl;
     private $_ticket;
     
-    public function __construct($url, $ticket = null)
-    {
+    public function __construct($url, $ticket = null) {
         $this->setBaseUrl($url);
         if (isset($ticket)) {
             $this->setTicket($ticket);
         }
     }
     
-    public function getBaseUrl()
-    {
+    public function getBaseUrl() {
         return $this->_alfrescoBaseUrl;
     }
     
-    public function setBaseUrl($alfrescoBaseUrl)
-    {
+    public function setBaseUrl($alfrescoBaseUrl) {
         $this->_alfrescoBaseUrl = $alfrescoBaseUrl;
     }
     
-    public function getTicket()
-    {
+    public function getTicket() {
         return $this->_ticket;
     }
     
-    public function setTicket($ticket)
-    {
+    public function setTicket($ticket) {
         $this->_ticket = $ticket;
     }
     
     /*
      * Put an "alf_ticket=<ticket>" on the given URL
      */
-    public function addAlfTicketUrl($url)
-    {
+    public function addAlfTicketUrl($url) {
         $ticket = $this->getTicket();
         if (isset($ticket)) {
-            if (strstr($url, '?')) {
-                $url .= "&alf_ticket=" . $ticket;
-            } else {
-                $url .= "?alf_ticket=" . $ticket;
-            }
+            $url .= (strstr($url, '?')) ? '&' : '?';
+            $url .= "alf_ticket=" . $ticket;
         }
+        
         return $url;
     }
     
-    public function isAlfrescoError($return)
-    {
+    public function isAlfrescoError($return) {
         if (is_array($return) && isset($return['exception'])) {
             return true;
         }
         return false;
     }
     
-    public function getAlfrescoErrorMessage($return)
-    {
+    public function getAlfrescoErrorMessage($return) {
         if ($this->isAlfrescoError($return)) {
             return $return['message'];
         }
     }
     
-    protected function _getResultFromUrl($url)
-    {
-        $result = $this->_getCurlClient()->doGetRequest($url);
-        
-        if ($this->isAlfrescoError($result)) {
-            throw new Exception($this->getAlfrescoErrorMessage($result));
-        }
-        
-        return $result;
-    }
-    
-    protected function _doPostRequest($url, $postData)
-    {
+    protected function _doPostRequest($url, $postData) {
     	$result = $this->_getCurlClient()->doPostRequest($url, $postData);
     	 
     	if ($this->isAlfrescoError($result)) {
@@ -90,9 +80,8 @@ abstract class Alfresco_Rest_Abstract
     	return $result;
     }
     
-    protected function _doPostFormDataRequest($url, $postData)
-    {
-    	$result = $this->_getCurlClient()->doPostRequest($url, $postData, 'formdata');
+    protected function _doPostFormDataRequest($url, $postData) {
+    	$result = $this->_getCurlClient()->doPostRequest($url, $postData, Alfresco_Rest_CurlClient::FORMAT_FORMDATA);
     
     	if ($this->isAlfrescoError($result)) {
     		throw new Alfresco_Rest_Exception($this->getAlfrescoErrorMessage($result));
@@ -101,8 +90,7 @@ abstract class Alfresco_Rest_Abstract
     	return $result;
     }
     
-    protected function _doGetRequest($url)
-    {
+    protected function _doGetRequest($url) {
     	$result = $this->_getCurlClient()->doGetRequest($url);
     	 
     	if ($this->isAlfrescoError($result)) {
@@ -112,39 +100,27 @@ abstract class Alfresco_Rest_Abstract
     	return $result;
     }
     
-	protected function _doGetStringRequest($url)
-    {
-    	$result = $this->_getCurlClient()->doGetRequest($url, Alfresco_Rest_CurlClient::FORMAT_STRING);
-    	 
-    	return $result;
+	protected function _doGetStringRequest($url) {
+    	return $this->_getCurlClient()->doGetRequest($url, Alfresco_Rest_CurlClient::FORMAT_STRING);
     }
     
-    protected function _doAuthenticatedPostRequest($url, $postData)
-    {
-    	$url = $this->addAlfTicketUrl($url);
-    	return $this->_doPostRequest($url, $postData);
+    protected function _doAuthenticatedPostRequest($url, $postData) {
+    	return $this->_doPostRequest($this->addAlfTicketUrl($url), $postData);
     }
     
-    protected function _doAuthenticatedPostFormDataRequest($url, $postData)
-    {
-    	$url = $this->addAlfTicketUrl($url);
-    	return $this->_doPostFormDataRequest($url, $postData);
+    protected function _doAuthenticatedPostFormDataRequest($url, $postData) {
+    	return $this->_doPostFormDataRequest($this->addAlfTicketUrl($url), $postData);
     }
     
-    protected function _doAuthenticatedGetRequest($url)
-    {
-    	$url = $this->addAlfTicketUrl($url);
-    	return $this->_doGetRequest($url);
+    protected function _doAuthenticatedGetRequest($url) {
+    	return $this->_doGetRequest($this->addAlfTicketUrl($url));
     }
     
-    protected function _doAuthenticatedGetStringRequest($url)
-    {
-    	$url = $this->addAlfTicketUrl($url);
-    	return $this->_doGetStringRequest($url);
+    protected function _doAuthenticatedGetStringRequest($url) {
+    	return $this->_doGetStringRequest($this->addAlfTicketUrl($url));
     }
     
-    protected function _getCurlClient()
-    {
+    protected function _getCurlClient() {
     	return new Alfresco_Rest_CurlClient();
     }
 }
--- Rest/Service.php
+++ Rest/Service.php
@@ -1,11 +1,6 @@
 <?php
 class Alfresco_Rest_Service extends Alfresco_Rest_Abstract
 {
-    public function getResultFromUrl($url)
-    {
-        return $this->_getResultFromUrl($url);
-    }
-    
     public function doPostRequest($url, $postData)
     {
     	return parent::_doPostRequest($url, $postData);
